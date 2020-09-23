7efb083fea910af75db1c0c6808b919f546cdccd
Chaudhary Sarimurrab <samurrab@gmail.com>
2020-09-23 12:57:53 +0530

    ll4

--- 31 LinkedList/2_insertion.cpp
+++ 31 LinkedList/2_insertion.cpp
@@ -21,64 +21,63 @@ void insertAtHead(node *&head, int data)
         head = new node(data);
         return;
     }
-    node * temp = new node(data);
-    temp ->next = head;
-    head = temp ;
+    node *temp = new node(data);
+    temp->next = head;
+    head = temp;
 }
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //~~~~~~~~~~~~~~~~~ length of LL~~~~~~~~~~~~~~~
-int length(node * head)
+int length(node *head)
 {
-    int cnt =0;
-    while(head!=NULL)
+    int cnt = 0;
+    while (head != NULL)
     {
-        cnt +=1;
+        cnt += 1;
         head = head->next;
     }
     return cnt;
 }
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-void insertAtTail(node *head,int data)
+void insertAtTail(node *head, int data)
 {
-    
-    while(head->next != NULL)
+
+    while (head->next != NULL)
     {
         head = head->next;
     }
     head->next = new node(data);
 }
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-void insertAtmid(node * head, int data, int p)    // After p position
+void insertAtmid(node *head, int data, int p) // After p position
 {
-    if(head==NULL || p==0)
+    if (head == NULL || p == 0)
     {
         insertAtHead(head, data);
         return;
     }
-    else if (p>length(head))
+    else if (p > length(head))
     {
-        insertAtTail(head,data);
+        insertAtTail(head, data);
     }
     else
     {
-        int n1= p-1;
-        while(n1!=0)
+        int n1 = p - 1;
+        while (n1 != 0)
         {
-            head = head ->next;
+            head = head->next;
             n1--;
         }
-        node * temp = new node(data);
+        node *temp = new node(data);
         temp->next = head->next;
         head->next = temp;
     }
-    
 }
 
 void display(node *head)
 {
-    while (head!= NULL)
+    while (head != NULL)
     {
-        cout << head->data<<" ";
+        cout << head->data << " ";
         head = head->next;
     }
 }
@@ -86,17 +85,17 @@ void display(node *head)
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-void deleteLL(node *& head, int pos)
+void deleteLL(node *&head, int pos)
 {
-    if(pos<=1)
+    if (pos <= 1)
     {
-        head = head ->next;
+        head = head->next;
     }
     else if (pos >= length(head))
     {
-        node * tmp = head;
-        int n2 = length(head)-2;
-        while(n2!=0)
+        node *tmp = head;
+        int n2 = length(head) - 2;
+        while (n2 != 0)
         {
             tmp = tmp->next;
             n2--;
@@ -106,98 +105,111 @@ void deleteLL(node *& head, int pos)
     else
     {
 
-        node * tmp = head;
+        node *tmp = head;
 
-        int jump = pos-2;
-        while(jump!=0)
+        int jump = pos - 2;
+        while (jump != 0)
         {
             tmp = tmp->next;
             jump--;
         }
-        node * x = tmp->next;
-        node * x1 = x->next;
+        node *x = tmp->next;
+        node *x1 = x->next;
         tmp->next = x1;
-
-
     }
-    
 }
 
 // **********************************
-void delete_inplace(node * & head)
+void delete_inplace(node *&head)
 {
-    if(head == NULL)
-    return;
+    if (head == NULL)
+        return;
 
-    node * temp = head->next;
-    delete head; 
-//    ^  It will delete the address, that head is pointing too
-//    |  It wont't delete head variable
+    node *temp = head->next;
+    delete head;
+    //    ^  It will delete the address, that head is pointing too
+    //    |  It wont't delete head variable
     head = temp;
-
 }
 // **********************************
-bool search(node*head, int key) // linear search
+bool search(node *head, int key) // linear search
 {
-    while(head!=NULL)
+    while (head != NULL)
     {
-        if(head->data==key)
+        if (head->data == key)
         {
             return true;
         }
-        head=head->next;
+        head = head->next;
     }
     return false;
 }
 // **********************************
-bool recursive_search(node * head, int key)
+bool recursive_search(node *head, int key)
 {
-    
-    if(head == NULL)
-    return false;
 
-    if(head->data == key)
-    return true;
+    if (head == NULL)
+        return false;
+
+    if (head->data == key)
+        return true;
     else
-    return recursive_search(head->next,key);
+        return recursive_search(head->next, key);
 }
 // ******************************
-// read inut from a file 
+// read inut from a file
 // g++ -o anyname abc.cpp
 // srm < input.txt
-node* input_linked()
+node *input_linked()
 {
     int d;
-    cin>>d;
+    cin >> d;
     node *head = NULL;
-    while(d!=-1)
+    while (d != -1)
     {
-        insertAtHead(head,d);
-        cin>>d;
+        insertAtHead(head, d);
+        cin >> d;
     }
     return head;
-
 }
-ostream& operator<<(ostream &ae, node * head)
+ostream &operator<<(ostream &ae, node *head)
 {
     display(head);
-    cout<<endl;
+    cout << endl;
     return ae;
-
 }
-istream& operator>>(istream& cin, node * &head)
+istream &operator>>(istream &cin, node *&head)
 {
     head = input_linked();
     return cin;
-
 }
+
+void reverseiterate(node *&head)
+{
+    node *prev = NULL;
+    node *curr = head;
+    node *n;
+    while (curr != NULL)
+    {
+        n = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = n;
+    }
+    head = prev;
+    
+}
+
 int main()
 {
     // node * head = input_linked();
     // node * head2 = input_linked();
-    node * head,* head2;
-    cin>>head>>head2;
-    cout<<head<<head2;
+    node *head, *head2;
+    cin >> head >> head2;
+    cout << head << head2;
+    reverseiterate(head2);
+    cout<<endl;
+    cout<<head2;
     //display(head);
     /*
     node *head = NULL;
