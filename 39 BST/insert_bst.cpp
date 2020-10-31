079831a1ea7b3199ab0c6edb6cbb867c0d6bfbe4
Chaudhary Sarimurrab <samurrab@gmail.com>
2020-11-01 01:45:42 +0530

    COMMIT BY SARIMs BOT

--- 39 BST/insert_bst.cpp
+++ 39 BST/insert_bst.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 class node
 {
-    public:
+public:
     int data;
-    node * left;
-    node * right;
+    node *left;
+    node *right;
     node(int d)
     {
         data = d;
@@ -15,58 +15,103 @@ class node
     }
 };
 
-
-node * insert_bst(node * root, int d)
+node *insert_bst(node *root, int d)
 {
-    if(root == NULL)
-    return new node(d);
+    if (root == NULL)
+        return new node(d);
 
-
-
-
-    if(d<= root->data )
-    root->left = insert_bst(root->left,d);
+    if (d <= root->data)
+        root->left = insert_bst(root->left, d);
     else
-    root->right = insert_bst(root->right,d);
-    
+        root->right = insert_bst(root->right, d);
+
     return root;
-    
-
-
 }
 
-
-void print_inorder(node * root)
+void print_inorder(node *root)
 {
-    if(root ==NULL)
-    return;
+    if (root == NULL)
+        return;
 
     print_inorder(root->left);
-    cout<<root->data<<" ";
+    cout << root->data << " ";
     print_inorder(root->right);
 
     return;
-
 }
 
-node * build_bst()
+node *build_bst()
 {
     int d;
-    cin>>d;
+    cin >> d;
 
-    node * root = NULL;
-    while(d!=-1)
+    node *root = NULL;
+    while (d != -1)
     {
-        root  = insert_bst(root,d);
-        cin>>d;
+        root = insert_bst(root, d);
+        cin >> d;
     }
     return root;
 }
 
+node *delete_bst(node *root, int key)
+{
+    if (root == NULL)
+        return NULL;
+
+    if (root->data == key)
+    {
+        //1 - NO child
+        if (root->left == NULL and root->right == NULL)
+        {
+            delete root;
+            return NULL;
+        }
+        //2 - One child on either side
+        else if (root->left != NULL and root->right == NULL)
+        {
+            node *temp = root->left;
+            delete root;
+            return temp;
+        }
+        else if (root->right != NULL and root->left == NULL)
+        {
+            node *temp = root->right;
+            delete root;
+            return temp;
+        }
+        //3 - two childs
+        else
+        {
+            node *replace = root->right;
+            while (replace->left != NULL)
+            {
+                replace = replace->left;
+            }
+            //replace is the node with which we have to replace
+
+            root->data = replace->data;
+            root->right = delete_bst(root->right, root->data);
+            return root;
+        }
+    }
+    else if (key < root->data)
+    {
+        root->left = delete_bst(root->left, key);
+        return root;
+    }
+    else
+    {
+        root->right = delete_bst(root->right, key);
+        return root;
+    }
+}
+
 int main()
 {
-    node * root = build_bst();
+    node *root = build_bst();
     print_inorder(root);
-
-
+    cout << endl;
+    delete_bst(root, 2);
+    print_inorder(root);
 }
